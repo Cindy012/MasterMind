c73bb5f8ed60ca01005c178f6739716b536ce4ce
Cindy012 <74193681+Cindy012@users.noreply.github.com>
2022-07-19 22:01:56 +0200

    Reverted App component

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import MasterMind from './Components/MasterMind';
+
+function App() {
+	return (
+    	<MasterMind />
+	);
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import MasterMind from './Components/MasterMind';
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
 );
 root.render(
 	<React.StrictMode>
-		<MasterMind />
+		<App />
 	</React.StrictMode>
 );
 
