93a3c0ae6212f6fbde80ef9502703a0107cf1193
Cindy012 <74193681+Cindy012@users.noreply.github.com>
2022-07-21 21:58:10 +0200

    Removed 2 color pegs and fixed styling

--- src/Components/Color.ts
+++ src/Components/Color.ts
@@ -2,10 +2,8 @@ export enum Color {
     Red = "red",
     Orange = "orange",
     Green = "green",
-    LightBlue = "lightblue",
     Blue = "blue",
     Purple = "purple",
-    Rose = "rose",
     Yellow = "yellow",
     Black = "black",
     White = "white"
