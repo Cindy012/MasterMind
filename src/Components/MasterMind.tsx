9cf1db6c56c0fa6c42cfc9ebaeb6f041ba531039
Cindy012 <74193681+Cindy012@users.noreply.github.com>
2022-07-21 14:03:27 +0200

    Implemented game info modal

--- src/Components/MasterMind.tsx
+++ src/Components/MasterMind.tsx
@@ -1,14 +1,29 @@
+import { Fragment, useState } from "react";
+import Modal from "../Modal/Modal";
 import Board from "./Board";
 
 const MasterMind = () => {
-	const textInfo = "Mastermind is a game to guess a secret code that exists of 4 pegs. The player has max 12 chances to guess the color code.";
+	const modalTitle = "Mastermind game info"
+	const [showModal, setShowModal] = useState<boolean>(false);
+	const gameInfo = "Mastermind is a game to guess a secret code that exists of 4 pegs. The player has max 8 chances to guess the color code.";
   
 	return (
-		<div className="mastermind">
-			<h1>MasterMind</h1>
-			<section>{ textInfo }</section>
-			<Board />
-		</div>
+		<Fragment>
+			<div className="mastermind">
+				<h1>MasterMind</h1>
+				<button className="button__icon" onClick={ () => setShowModal(true) }>
+					<img className="image__icon" src={ require('../image/information.png') } alt=""/>
+				</button>
+				<Board />
+			</div>
+			<Modal
+				title= { modalTitle }
+				show={ showModal }
+				setShowModal={ setShowModal }
+				content= { gameInfo }
+				hideCloseButton
+			/>
+		</Fragment>
 	);
 };
 
