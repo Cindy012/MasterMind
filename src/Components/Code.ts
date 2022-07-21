d31f1eec7fa30537df5db98c82d3457bc17409a2
Cindy012 <74193681+Cindy012@users.noreply.github.com>
2022-07-21 20:10:48 +0200

    Fixed generating code

--- src/Components/Code.ts
+++ src/Components/Code.ts
@@ -1,7 +1,7 @@
 import { Color } from "./Color";
 
 function randomEnum<T>(anEnum: T): T[keyof T] {
-    const enumValues = (Object.keys(anEnum) as unknown) as T[keyof T][];
+    const enumValues = (Object.values(anEnum) as unknown) as T[keyof T][];
     const randomIndex = Math.floor(Math.random() * enumValues.length);
     return enumValues[randomIndex];
   }
@@ -11,7 +11,7 @@ export function createCode(pegs: number) {
     let color;
     while (pegs > 0 ) {
         color = randomEnum(Color);
-        if (color.toString() !== 'Black' || color.toString() !== 'White') {
+        if (color.toString() !== 'black' || color.toString() !== 'white') { // if stmt is not yet done
             code.push(color);
             pegs--;
         }
