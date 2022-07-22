f3f0db3087a921d3eba4bd283b4d456525efe5e0
Cindy012 <74193681+Cindy012@users.noreply.github.com>
2022-07-22 14:54:31 +0200

    Improve game info Mastermind

--- src/Components/MasterMind.tsx
+++ src/Components/MasterMind.tsx
@@ -9,9 +9,9 @@ const MasterMind = () => {
 	const getGameInfo = () => {
 		return (
 			<article>
-				<p>Mastermind is a game to guess a secret code that exists on 4 pegs. The player has 8 chances to guess the color code.</p>
-				<p>To give the pegs a color, select from the side of the board one of the color pegs. After selecting a color the player can fill the row of the board.</p>
-				<p>When the player selected a code pattern, the code will be reviewed and provides feedback through clues. The black peg means that the chosen peg color and position is correct. The red peg means that the peg color contains in the code although the position is not.</p>
+				<p>MasterMind is a game whereby the user guesses code on four pegs. The player has eight chances to guess the color code.</p>
+				<p>To choose a color, select from the side one of the colors pegs. After selecting the color, the player can fill the row of the board.</p>
+				<p>When the player selects a code pattern, the code will be reviewed and provides feedback through clues. The black peg means that the chosen peg color and position are correct. The red peg means that the peg color retains in the code although the position is not.</p>
 			</article>
 		);
 	};
