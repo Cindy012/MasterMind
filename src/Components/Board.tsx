8cd4a3edc53d16dbd27a9f612f0a03cefc6ff1ec
Cindy012 <74193681+Cindy012@users.noreply.github.com>
2022-07-23 12:43:42 +0200

    Adjusted modal to reduce code in board

--- src/Components/Board.tsx
+++ src/Components/Board.tsx
@@ -15,27 +15,27 @@ const Board = () => {
 	const [turn, setTurn] = useState<number>(0);
 	const [code, setCode] = useState<Color[]>(createCode(pegsInRow));
 	const [currentColor, setCurrentColor] = useState<Color>();
-	const [gameStatus, setGameStatus] = useState<number>(0); // 0: not filled, 1: winner, 2: loser
+	const [gameStatus, setGameStatus] = useState<number>(0); // 0: gameInfo, 1: not filled/ongoing, 2: winner, 3: loser
 	const [board, setBoard] = useState({ gameBoard: initializeBoard(totalRows, pegsInRow), clueBoard: initializeCluesBoard(totalRows, pegsInRow)});
 	const [boardView, setBoardView] = useState<JSX.Element[]>();
 	const [cluesBoardView, setCluesBoardView] = useState<JSX.Element[]>();
 	const [colorPegOptionsView, setColorPegOptionsView] = useState<JSX.Element[]>();
-	const [modalTitle, setModalTitle] = useState<string>();
-	const [showModal, setShowModal] = useState({ showGameStateModal: false, showGameInfoModal: false });
+	const [showModal, setShowModal] = useState(false);
 
 	const selectCurrentColor = (color: Color) => setCurrentColor(color);
+	const openModal = () => setShowModal(true);
 
 	const resetGame = () => {
 		setCode(createCode(pegsInRow));
 		setTurn(0);
 		setBoard({ gameBoard: initializeBoard(totalRows, pegsInRow), clueBoard: initializeCluesBoard(totalRows, pegsInRow) });
-		setGameStatus(0);
+		setGameStatus(1);
 	};
 
 	const checkCode = () => {
 		if (turn < totalRows && board.gameBoard[turn].includes(Color.White)) {
-			setModalTitle('Not yet');
-			setShowModal({ showGameStateModal: true, showGameInfoModal: false })
+			setGameStatus(1);
+			openModal();
 		} else if (turn < totalRows) {
 			let newCluesBoard = board.clueBoard;
 			code.forEach((color, index) => {
@@ -46,22 +46,25 @@ const Board = () => {
 				}
 			});
 			setBoard({ gameBoard: board.gameBoard, clueBoard: [...newCluesBoard] });
-			isGameOver() ? setShowModal({ showGameStateModal: true, showGameInfoModal: false })  : setTurn(turn + 1);
+			isGameOver() ? openModal() : setTurn(turn + 1);
 		}
 	};
 
 	const isGameOver = () => {
 		if (isCodeCorrect(code, board.gameBoard[turn]) && turn < totalRows ) {
-			setModalTitle('Winner!');
-			setGameStatus(1);
+			setGameStatus(2);
 			return true;
 		} else if (board.gameBoard[turn] !== code && turn === totalRows - 1) {
-			setModalTitle('Loser!');
-			setGameStatus(2);
+			setGameStatus(3);
 			return true;
 		}
 		return false;
 	};
+
+	function openGameInfoModal(): void {
+		setGameStatus(0);
+		openModal();
+	}
 
 	useEffect(() => {
 		const colorPeg = (rowId: number, pegId: number) => {
@@ -161,7 +164,7 @@ const Board = () => {
 				<button className="button__icon" onClick={ () => resetGame() }>
 					<img className="image__icon" src={ require('../image/play-again.png') } alt=""/>
 				</button>
-				<button className="button__icon" onClick={ () => setShowModal({ showGameStateModal: false, showGameInfoModal: true }) }>
+				<button className="button__icon" onClick={ () => openGameInfoModal() }>
 					<img className="image__icon" src={ require('../image/information.png') } alt=""/>
 				</button>
 			</div>
@@ -175,20 +178,13 @@ const Board = () => {
 				</div>
 			</div>
             <Modal
-				title={ modalTitle }
-                show={ showModal.showGameStateModal }
+                show={ showModal}
                 setShowModal={ setShowModal }
 				gameStatus={ gameStatus }
 				resetGame={ () => resetGame() }
+				gameInfo={ getGameInfo() }
             	hideCloseButton
             />
-			<Modal
-				title={ 'Mastermind game info' }
-				show={ showModal.showGameInfoModal }
-				setShowModal={ setShowModal }
-				gameInfo={ getGameInfo() }
-				hideCloseButton
-			/>
 		</Fragment>
 	);
 };
--- src/Modal/Modal.tsx
+++ src/Modal/Modal.tsx
@@ -1,40 +1,40 @@
 import confetti from 'canvas-confetti';
-import { Dispatch, SetStateAction, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 interface ModalProps {
-    setShowModal: Dispatch<SetStateAction<{ showGameStateModal: boolean; showGameInfoModal: boolean; }>>;
+    setShowModal: React.Dispatch<React.SetStateAction<boolean>>;
     hideCloseButton?: boolean;
     show: boolean;
-    title: string | undefined;
     resetGame?: () => void
-    gameStatus?: number;
+    gameStatus: number; // 0: gameInfo, 1: not filled/ongoing, 2: winner, 3: loser
     gameInfo?: JSX.Element;
 }
 
-const Modal:React.FC<ModalProps> = ({ title, setShowModal, show, hideCloseButton, resetGame, gameStatus, gameInfo }) => {
+const Modal:React.FC<ModalProps> = ({ setShowModal, show, hideCloseButton, resetGame, gameStatus, gameInfo }) => {
     const [playConfetti, setPlayConfetti] = useState(false);
     const playAgain = () => {
         if (resetGame) {
             setPlayConfetti(false);
             resetGame();
-            setShowModal({ showGameStateModal: false, showGameInfoModal: false });
+            setShowModal(false);
         }
     };
 
-    const renderSwitch = (state: number | undefined) => {
-        switch(state) {
-            case 0:
-                return 'You did not give the code colors.';
-            case 1:
-                return 'You win, play again!';
-            case 2:
-                return 'You lose, next time better!';
-            default:
-                return null;
-        }
-    };
+    const title: string[] = [
+        'Mastermind game info',
+        'Not Yet!',
+        'Winner!',
+        'Loser!'
+    ];
 
-    if (gameStatus === 1 && resetGame && !playConfetti) {
+    const text: string[] = [
+        '',
+        'You did not give the code colors.',
+        'You win, play again!',
+        'You lose, next time better!'
+    ];
+
+    if (gameStatus === 2 && resetGame && !playConfetti) {
         setPlayConfetti(true);
     }
 
@@ -51,15 +51,14 @@ const Modal:React.FC<ModalProps> = ({ title, setShowModal, show, hideCloseButton
     return (
         <div className={`modal ${show ? 'active' : ''}`}>
             <div className={ gameInfo ? 'modal__content' : 'modal__content modal__content__small' }>
-                { !hideCloseButton && <span onClick={() => setShowModal({ showGameStateModal: false, showGameInfoModal: false })} className="modal__close">&times;</span> }
-                <h2>{ title ? title : 'Something went wrong' }</h2> 
-                <p>{ renderSwitch(gameStatus) }</p>
-                { gameInfo && gameInfo }
-                <div className={ gameStatus !== 0 && !gameInfo ? 'modal__footer' : 'modal__footer__secondary'}>
-                    { gameStatus !== 0 && !gameInfo ? (
+                { !hideCloseButton && <span onClick={() => setShowModal(false)} className="modal__close">&times;</span> }
+                <h2>{ title[gameStatus] }</h2> 
+                { gameInfo && gameStatus === 0 ? gameInfo : <p>{ text[gameStatus] }</p> }
+                <div className={ gameStatus === 0 || gameStatus === 1 ? 'modal__footer__secondary' : 'modal__footer'}>
+                    { gameStatus !== 1 && gameStatus !== 0 ? (
                         <button className="modal__button" onClick={ () => playAgain() }>Play again!</button>
                     ) : null }
-                    <button className="modal__button" onClick={ () => setShowModal({ showGameStateModal: false, showGameInfoModal: false }) }>Close</button>
+                    <button className="modal__button" onClick={ () => setShowModal(false) }>Close</button>
                 </div>
             </div>
         </div>
