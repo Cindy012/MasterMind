1e88fc8755016cc165c63ffaad2cc67b71b9e480
Cindy012 <74193681+Cindy012@users.noreply.github.com>
2022-07-26 11:11:45 +0200

    Update MasterMind.tsx

--- src/Components/MasterMind.tsx
+++ src/Components/MasterMind.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Modal from "../Modal/Modal";
 import { Color, createCode, getClues, initializeBoard, isCodeCorrect, pegsInRow, totalRows } from "../ts/Game";
 import CluePeg from "./CluePeg";
